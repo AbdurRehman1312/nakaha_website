db5ebb46fc2ee3df5b0fd3dfe0609d8c7ee35458
AbdurRehman1312 <abdurrehmansaifi784@gmail.com>
2024-05-01 00:16:37 +0500

    react router dom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,9 @@ import * as images from "../assets"
 const Footer = () => {
   return (
     <footer className='bg-primary p-5'>
+        <div>
+
+        </div>
         <img src={images.logo} alt="" className='w-[10%]' />
     </footer>
   )
